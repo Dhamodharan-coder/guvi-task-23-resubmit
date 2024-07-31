181cc795bf18af2084090aa84a494d9471963c44
Dhamodharan-coder <dhamodharanmunus10@gmail.com>
2024-08-01 00:32:05 +0530

    adding all task files

---
+++ src/Heading.jsx
@@ -0,0 +1,8 @@
+function Heading(){
+    return <div className="heading d-flex flex-column justify-content-center align-items-center">
+<h1 className="text-light display-1">Shop in style</h1>
+<span className="text-light h4 text-center">With this shop hompeage template</span>
+    </div>
+}
+
+export default Heading
